e76d6e51691adfb8f6229f7c2b59929e6b674daa
Joshua Tompkins <josh@joshtompkins.com>
2018-09-03 15:20:00 -0700

    Add some CLI parsing!

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"joshtompkins.com/elementary-automata/automata"
 	"joshtompkins.com/elementary-automata/evaluators"
 	"joshtompkins.com/elementary-automata/generation"
@@ -9,21 +10,32 @@ import (
 	"joshtompkins.com/elementary-automata/rules"
 )
 
+var (
+	size  = kingpin.Flag("size", "Size of each generation").Short('s').Default("1000").Int()
+	gens  = kingpin.Flag("generations", "Number of generations to simulate").Short('g').Default("1000").Int()
+	scale = kingpin.Flag("scale", "Size of each cell in the output in pixels").Default("1").Int()
+	rule  = kingpin.Arg("rule", "Wolfram rule to use").Required().Int()
+	file  = kingpin.Arg("file", "Path to output PNG").Required().String()
+)
+
 func main() {
+	kingpin.Version("0.1")
+	kingpin.Parse()
+
+	r, _ := rules.Get(*rule)
+
 	a := automata.New(
-		generation.NewFromCenter(1000),
-		evaluators.NewRuleEvaluator(rules.OneTwentySix),
+		generation.NewFromRandom(*size),
+		evaluators.NewRuleEvaluator(r),
 		renderers.NewPngRenderer(&renderers.RenderOptions{
-			File:  "test.png",
-			Scale: 1,
+			File:  *file,
+			Scale: *scale,
 		}),
 	)
 
-	log.SetLevel(log.DebugLevel)
-
 	log.Info("Evaluating rules")
 
-	for i := 1; i < 1001; i++ {
+	for i := 1; i <= *size; i++ {
 		a.Step()
 	}
 
